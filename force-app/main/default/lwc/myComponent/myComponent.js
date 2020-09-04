e7a53968db7ae6ff46ad711eed7f3b701167d958
Robert Sösemann <robert.soesemann@logicline.de>
2020-09-04 13:31:48 +0200

    Add LWC issues

--- force-app/main/default/lwc/myComponent/myComponent.js
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -1,10 +1,15 @@
 import { LightningElement } from "lwc";
 
 export default class myComponents extends LightningElement {
+
     connectedCallback() {
         setTimeout(() => {
             const el = this.template.querySelector('span');
             // ... manipulating the span LightningElement
         });
+    }
+
+    renderedCallback() {
+        const item = document.querySelector('.my-item');
     }
 }
