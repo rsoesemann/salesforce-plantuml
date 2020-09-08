f7a5662561ccdb15229163f8f9f63dacdb2352de
Robert Sösemann <robert.soesemann@logicline.de>
2020-09-08 10:12:06 +0200

    Ruleset adjustments

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -1,50 +1,29 @@
 #!/bin/bash
 source `dirname $0`/config.sh
 
-holdScreen=true
-while getopts 'p' flag; do
-  case "${flag}" in
-    p) holdScreen=false ;;
-  esac
-done
-
 execute() {
-    $@ > temp.json
-    cat temp.json
-    $(jq '.status == 0' temp.json) || $holdScreen || exit 1
+  $@ || exit
 }
 
-executenpm() {
-    $@
-    $(jq '.success' temp.json) || $holdScreen || exit 1
-}
+if [ -z "$DEV_HUB_URL" ]; then
+  echo "set default devhub user"
+  execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
 
-
-echo "deleting old scratch org"
-sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
-
-echo "Creating scratch ORG"
-sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 1
-
-echo "Pushing changes to scratch org"
-execute "sfdx force:source:push --json"
-
-echo "Assigning permission"
-execute "sfdx force:user:permset:assign --json -n Admin"
-
-echo "Running apex tests"
-execute "sfdx force:apex:test:run -l RunLocalTests -w 30 --json"
-
-
-echo "Running jest tests"
-npm install
-executenpm "npm run test:unit"
-
-rm temp.json
-
-if $holdScreen 
-then
-    echo "Org is set up"
-    read
+  echo "deleting old scratch org"
+  sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
 fi
 
+echo "Creating scratch ORG"
+execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 30
+
+echo "Pushing changes to scratch org"
+execute sfdx force:source:push
+
+echo "Assigning permission"
+execute sfdx force:user:permset:assign -n PlantUml
+
+echo "Make sure Org user is english"
+sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
+
+echo "Running apex tests"
+execute sfdx force:apex:test:run -l RunLocalTests -w 30
